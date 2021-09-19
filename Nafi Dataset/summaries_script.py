bbb4d6a39a2357a38290f78f551efabb84f83c70
nrob6757 <nrob6757@uni.sydney.edu.au>
2021-09-19 15:27:34 +1000

    Modified Scripts for deaths

--- Nafi Dataset/summaries_script.py
+++ Nafi Dataset/summaries_script.py
@@ -2,7 +2,6 @@ import os
 import csv
 import datetime
 folder = os.path.dirname(os.path.realpath(__file__))
-days_count = 560
 
 def date_to_readable_str(input_date) -> str:
     values = input_date.split("/")
@@ -12,18 +11,13 @@ def date_to_readable_str(input_date) -> str:
     date_val = datetime.date(year,month,day)
     return date_val.strftime("%d-%b") + "-" +  date_val.strftime("%Y")
     
-def reformat_date(input_date) -> str:
-    """Reformats the date MM/DD/YY or MM/DD/YYY to DD/MM/YYYY"""
-    date_split = input_date.split("/")
-    year = date_split[2] if len(date_split[2]) > 2 else "20" + date_split[2]
-    return f"{date_split[1]}/{date_split[0]}/{year}"
 
 total_deaths_by_country = {} # Key: 'Country', Value: No. of Deaths
 new_deaths_by_country = {} # Key: 'Country', Value:{ 'Date' : No of Deaths}
 cumulative_death_by_country = {} # Key: 'Country', Value:{ 'Date' : No of Deaths}
 headers = []
 is_first = True
-csv_reader = csv.reader(open(f"{folder}\\time_series_covid19_deaths_global.csv"))
+csv_reader = csv.reader(open(f"{folder}\\deaths_cleaned.csv"))
 for values in csv_reader:
     if is_first:
             is_first = False
@@ -34,18 +28,18 @@ for values in csv_reader:
         if(country != "Australia" and country != "US" and country != "United Kingdom" and country != "India"):
             continue
         if country not in total_deaths_by_country:
-            total_deaths_by_country[country] = int(values[-1])
+            total_deaths_by_country[country] = int(values[-3])
         else:
-            total_deaths_by_country[country] += int(values[-1])
+            total_deaths_by_country[country] += int(values[-3])
 
         if country not in cumulative_death_by_country:
-                cumulative_death_by_country[country] = {}
-        for day_index in range(4, len(values)):
-            date_str = reformat_date(headers[day_index])
-            if date_str not in cumulative_death_by_country[country]:
-                cumulative_death_by_country[country][date_str] = int(values[day_index])
+            cumulative_death_by_country[country] = {}
+        #Go from dates 22-Jan-2020 to 10-September-2021
+        for date_index in range(4, len(values) - 2):
+            if headers[date_index] not in cumulative_death_by_country[country]:
+                cumulative_death_by_country[country][headers[date_index]] = int(values[date_index])
             else:
-                cumulative_death_by_country[country][date_str] += int(values[day_index])
+                cumulative_death_by_country[country][headers[date_index]] += int(values[date_index])
 
 
 
@@ -61,7 +55,7 @@ for country in cumulative_death_by_country.keys():
 
 
 
-print(f"From {date_to_readable_str(reformat_date(headers[4]))} to {date_to_readable_str(reformat_date(headers[-1]))}")
+print(f"From {date_to_readable_str(headers[4])} to {date_to_readable_str(headers[-3])}")
 for country in total_deaths_by_country.keys():
     deaths = "{:,}".format(total_deaths_by_country[country])
     print(f"{country} had {deaths} deaths")
