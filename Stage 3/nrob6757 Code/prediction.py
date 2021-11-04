be990ea1b6557a300bfb8bf7a1513e10c1874e68
Nafi Robayat <nafi@live.com>
2021-11-04 16:47:18 +1100

    Added stage 3 files

--- Stage 3/nrob6757 Code/prediction.py
+++ Stage 3/nrob6757 Code/prediction.py
@@ -1,32 +1,46 @@
 import pandas as pd
 import os
 from math import sqrt
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 from sklearn import metrics
-from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 
 folder = os.path.dirname(os.path.realpath(__file__))
 
-
 df = pd.read_csv(folder + '/integrate_dataset.csv')
-X = df[["Containment Index", "New Cases"]]     # slice dataFrame for input variables
-y = df['New Deaths']        # slice dataFrame for target variable
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-neigh = neighbors.KNeighborsRegressor(n_neighbors=4).fit(X_train, y_train)
 
-# Let's create one sample and predict the number of comments
-sample = [200, 200]        # a sample with 1000 likes and 100 dislikes
-sample_pred = neigh.predict([sample])
-print('----- Sample case -----')
-print("likes:",sample[0])
-print("dislikes:",sample[1])
-print('Predicted number of comments:', int(sample_pred))
-print('-----------------------')
+country_list = df["Country"].unique()
+country_group = df.groupby("Country")
 
-# Use the model to predict X_test
-y_pred = neigh.predict(X_test)
-# Root mean squared error
-mse = metrics.mean_squared_error(y_test, y_pred)
-print('Root mean squared error (RMSE):', sqrt(mse))
-# R-squared score: 1 is perfect prediction
-print('R-squared score:', metrics.r2_score(y_test, y_pred))
+for country_str in country_list:
+    country_df = country_group.get_group(country_str)
+    x = country_df.iloc[:,-2:-1]
+    y = country_df.iloc[:,8]
+    plt.scatter(x,y)
+
+
+
+for country_str in country_list:
+    country_df = country_group.get_group(country_str)
+    X = country_df.iloc[:,-2:-1]
+    Y = country_df.iloc[:,8]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    regr = linear_model.LinearRegression().fit(X_train, y_train)
+    sample = [80] 
+    sample_pred = regr.predict([sample])
+    print(f"===== {country_str} Statistics ====")
+    print(f"Predicted value : {sample_pred}")
+    print('Coefficients:')
+    print(regr.coef_)
+    # Use the model to predict y from X_test
+    y_pred = regr.predict(X_test)
+    # Root mean squared error
+    mse = metrics.mean_squared_error(y_test, y_pred)
+    print('Root mean squared error (RMSE):', sqrt(mse))
+    # R-squared score: 1 is perfect prediction
+    print('R-squared score:', metrics.r2_score(y_test, y_pred))
+
+plt.show()
