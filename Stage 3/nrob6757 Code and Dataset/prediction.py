47660e132bbda741825e69ff0e65f89eb7e3b9ee
Nafi Robayat <nafi@live.com>
2021-11-07 18:19:26 +1100

    Updated PCA prediction

--- Stage 3/nrob6757 Code and Dataset/prediction.py
+++ Stage 3/nrob6757 Code and Dataset/prediction.py
@@ -2,9 +2,10 @@ import pandas as pd
 import os
 from math import sqrt
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
 from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
 folder = os.path.dirname(os.path.realpath(__file__))
@@ -14,24 +15,35 @@ df = pd.read_csv(folder + '/integrate_dataset.csv')
 country_list = df["Country"].unique()
 country_group = df.groupby("Country")
 
-for country_str in country_list:
-    country_df = country_group.get_group(country_str)
-    x = country_df.iloc[:,-2:-1]
-    y = country_df.iloc[:,8]
-    plt.scatter(x,y)
-
-
 
 for country_str in country_list:
+    predictive_value = ["New Deaths"]
+    predictor_values = [
+        "People Vaccinated per 100",
+        "People Fully Vaccinated per 100",
+        "New Cases",
+        "Stringency Index"	
+        ]
     country_df = country_group.get_group(country_str)
-    X = country_df.iloc[:,-2:-1]
-    Y = country_df.iloc[:,8]
+    country_df = country_df.drop(columns=["Date","Country"])
+    X_df = country_df[predictor_values]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(X_df)
+
+    pca = PCA(n_components=1)
+
+    X = pca.fit_transform(scaled_data)
+    Y = country_df[predictive_value]
+
+    plt.scatter(X,Y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
     regr = linear_model.LinearRegression().fit(X_train, y_train)
-    sample = [80] 
+    sample = [3] 
     sample_pred = regr.predict([sample])
     print(f"===== {country_str} Statistics ====")
+    print(f"Standard Deviation Prediction: {sample}")
     print(f"Predicted value : {sample_pred}")
     print('Coefficients:')
     print(regr.coef_)
